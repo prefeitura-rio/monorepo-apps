bcab85e52de0a41d6223b178c5cd3711d2ba32b4
Victor Assis <victor_assis@poli.ufrj.br>
2024-11-23 13:11:43 -0300

    fix: :construction: Corrige erro na integração com o tailwind

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from "tailwindcss";
-import config from "@repo/ui/tailwind.config";
+import config from '@repo/ui/tailwind.config'
+import type { Config } from 'tailwindcss'
 
 const webConfig = {
   ...config,
@@ -8,19 +8,19 @@ const webConfig = {
     extend: {
       colors: {
         test: {
-          100: "#f2e8e5",
-          200: "#eaddd7",
-          300: "#e0cec7",
-          400: "#d2bab0",
-          500: "#bfa094",
-          600: "#a18072",
-          700: "#977669",
-          800: "#846358",
-          900: "#43302b",
+          100: '#f2e8e5',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
         },
       },
     },
   },
-} satisfies Config;
+} satisfies Config
 
-export default webConfig;
+export default webConfig
