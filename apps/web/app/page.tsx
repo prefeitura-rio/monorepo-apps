4c0967199e023130aae894a000ba9327892812e5
Victor Assis <victor_assis@poli.ufrj.br>
2024-12-02 09:12:58 -0300

    feat: :fire: Remove web app do template do monorepo

--- apps/web/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from '@repo/ui/molecules/button'
-
-export default function Home() {
-  return <Button variant="destructive">Click Me</Button>
-}
--- apps/web/app/vision-ai/page.tsx
+++
@@ -1,5 +0,0 @@
-import { VisionAi } from '@repo/modules/vision-ai'
-
-export default function Page() {
-  return <VisionAi />
-}
